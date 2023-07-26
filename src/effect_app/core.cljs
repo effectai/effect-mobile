76ade373abf197f89aa054b79e15e6af6fe2c213
Jesse Eisses <jesse.eisses@gmail.com>
2023-07-26 14:24:17 +0200

    Add basic refx (re-frame) structure

--- src/effect_app/core.cljs
+++ src/effect_app/core.cljs
@@ -1,11 +1,11 @@
 (ns effect-app.core
   (:require [uix.core :refer [defui $]]
-            [uix.dom]
             [react-native :refer [Text Button View TextInput SafeAreaView]]
+            [refx.alpha :as refx :refer [reg-event-fx dispatch use-sub]]
             ["@wharfkit/antelope" :refer [APIClient FetchProvider]]))
 
 (defui button [{:keys [on-click children]}]
-  ($ Button {:on-press on-click :title "load campaigns"}))
+  ($ Button {:on-press on-click :title children}))
 
 (def eos
   (-> (APIClient. #js {:provider (FetchProvider. "https://eos.greymass.com")})
@@ -14,12 +14,14 @@
 
 (defui main-screen []
   (let [[value set-value!] (uix.core/use-state "demo text field")
-        [camps set-camps!] (uix.core/use-state [])]
+        [camps set-camps!] (uix.core/use-state [])
+        total-ticks (use-sub [:total-ticks])]
     ($ View
        ($ TextInput {:value value
                      :on-change-text #(set-value! %)})
        ($ button {:on-click
                   #(do
+                     (dispatch [:click-load-campaigns])
                      (prn "loading campaigns...")
                      (.then
                       (.get_table_rows eos
@@ -29,11 +31,29 @@
                       (fn [res]
                         (let [campaigns (:rows (js->clj res
                                                         :keywordize-keys true))]
-                          (set-camps! campaigns)))))})
+                          (set-camps! campaigns)))))}
+          (str "load campaigns: " total-ticks))
        (for [c camps]
          ($ Text (str (get-in c [:owner 0]) " - " (get-in c [:owner 1])))))))
 
+(refx/reg-sub
+  :total-ticks
+  (fn [db _]
+    (:total-ticks db)))
+
+(reg-event-fx
+ :launch-app
+ (fn [{:keys [db]} _]
+   {:db {:current-path "/home"
+         :total-ticks 0}}))
+
+(reg-event-fx
+ :click-load-campaigns
+ (fn [{:keys [db]} _]
+   {:db (update db :total-ticks inc)}))
+
 (defn ^:export -main [& args]
+  (refx/dispatch-sync [:launch-app])
   ($ SafeAreaView
      ($ Text "bare bones demo of effect network native app")
      ($ main-screen)))
